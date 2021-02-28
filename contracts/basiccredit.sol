df9bbb5bc2b3fa244eb604e1dec593630a4909dc
Bhargav Aparoksham <aparokshambhargav@gmail.com>
2021-02-28 21:43:32 +0530

    Updated Basic Credit

--- contracts/basiccredit.sol
+++ contracts/basiccredit.sol
@@ -340,14 +340,14 @@ library Address {
      * ====
      */
     function isContract(address account) internal view returns (bool) {
-        // According to EIP-1052, 0x0 is the value returned for not-yet created accounts
-        // and 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470 is returned
-        // for accounts without code, i.e. `keccak256('')`
-        bytes32 codehash;
-        bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
+        // This method relies on extcodesize, which returns 0 for contracts in
+        // construction, since the code is only stored at the end of the
+        // constructor execution.
+
+        uint256 size;
         // solhint-disable-next-line no-inline-assembly
-        assembly { codehash := extcodehash(account) }
-        return (codehash != accountHash && codehash != 0x0);
+        assembly { size := extcodesize(account) }
+        return size > 0;
     }
 
     /**
@@ -379,11 +379,11 @@ library Address {
      * _Available since v2.4.0._
      */
     function sendValue(address payable recipient, uint256 amount) internal {
-        require(address(this).balance >= amount, "Address: insufficient");
+        require(address(this).balance >= amount, "Address: insufficient balance");
 
-        // solhint-disable-next-line avoid-call-value
-        (bool success, ) = recipient.call{value:amount}("");
-        require(success, "Address: reverted");
+        // solhint-disable-next-line avoid-low-level-calls, avoid-call-value
+        (bool success, ) = recipient.call.value(amount)("");
+        require(success, "Address: unable to send value, recipient may have reverted");
     }
 }
 
@@ -464,177 +464,178 @@ library SafeERC20 {
 contract BasicCredit is ReentrancyGuard {
     using SafeMath for uint;
     using SafeERC20 for IERC20;
+    using Address for address;
 
 
-	string public name = "Basic Credit Protocol"; 
+    string public name = "Basic Credit Protocol"; 
 
-	address public Admin;
+    address public Admin;
 
-	address[] public lenders;  //array addresses of lenders
+    address[] public lenders;  //array addresses of lenders
 
-	mapping(address => mapping(address => uint)) public depositBalance;
-	mapping(address => bool) public hasDeposited; // has deposited ever?
+    mapping(address => mapping(address => uint)) public depositBalance;
+    mapping(address => bool) public hasDeposited; // has deposited ever?
 
-	address[] public borrowers;  //array addresses of borrowers
+    address[] public borrowers;  //array addresses of borrowers
 
-	mapping(address => mapping(address => uint)) public borrowedAmount;
-	mapping(address => mapping(address => uint)) public creditLimit;
-	mapping(address => bool) public hasBorrowed; // has borrowed ever?
+    mapping(address => mapping(address => uint)) public borrowedAmount;
+    mapping(address => mapping(address => uint)) public creditLimit;
+    mapping(address => bool) public hasBorrowed; // has borrowed ever?
 
-	mapping(address => bool) public whitelistedToken; //If a ERC20 token is whitelisted
+    mapping(address => bool) public whitelistedToken; //If a ERC20 token is whitelisted
 
 
-	constructor() public {
+    constructor() public {
 
-		Admin = msg.sender;
+        Admin = msg.sender;
 
-	}
+    }
 
-	function whitelistToken(address _tokenaddress) public {
+    function whitelistToken(address _tokenaddress) public {
 
-		require(msg.sender == Admin, "Only Admin can call this function");
+        require(msg.sender == Admin, "Only Admin can call this function");
 
-		if(isContract(_tokenaddress)) {
+        if(_tokenaddress.isContract()) {
 
-			whitelistedToken(_tokenaddress) = true;
-		}
-	}
+            whitelistedToken[_tokenaddress] = true;
+        }
+    }
 
-	function removeWhitelist(address _tokenaddress) public {
+    function removeWhitelist(address _tokenaddress) public {
 
-		require(msg.sender == Admin, "Only Admin can call this function");
-		require(whitelistedToken(_tokenaddress) == true, "Token not yet whitelisted");
+        require(msg.sender == Admin, "Only Admin can call this function");
+        require(whitelistedToken[_tokenaddress] == true, "Token not yet whitelisted");
 
-		if(isContract(_tokenaddress)) {
+        if(_tokenaddress.isContract()) {
 
-			whitelistedToken(_tokenaddress) = false;
-		}
-	}
+            whitelistedToken[_tokenaddress] = false;
+        }
+    }
 
 
-	//Deposit Tokens - For Lenders
+    //Deposit Tokens - For Lenders
 
-	function DepositTokens(address _tokenaddress, uint _amount) public {
+    function DepositTokens(address _tokenaddress, uint _amount) public {
 
 
-		//require amount greater than 0. Require is a function in solidity.
-		require(_amount > 0, "Amount cannot be 0");
-		require(whitelistedToken(_tokenaddress) == true, "Only deposits of whitelisted tokens are accepted");
+        //require amount greater than 0. Require is a function in solidity.
+        require(_amount > 0, "Amount cannot be 0");
+        require(whitelistedToken[_tokenaddress] == true, "Only deposits of whitelisted tokens are accepted");
 
-		IERC20(_tokenaddress).safeTransferFrom(msg.sender, address(this), _amount);
+        IERC20(_tokenaddress).safeTransferFrom(msg.sender, address(this), _amount);
 
-		//Update deposit balance
+        //Update deposit balance
 
-		depositBalance[_tokenaddress][msg.sender] = depositBalance[_tokenaddress][msg.sender] + _amount;
+        depositBalance[_tokenaddress][msg.sender] = depositBalance[_tokenaddress][msg.sender] + _amount;
 
-		//Add user to lenders array if they haven't deposited already
+        //Add user to lenders array if they haven't deposited already
 
-		if(!hasDeposited[msg.sender]) {
+        if(!hasDeposited[msg.sender]) {
 
-			lenders.push(msg.sender);
-		}
+            lenders.push(msg.sender);
+        }
 
-		hasDeposited[msg.sender] = true;
+        hasDeposited[msg.sender] = true;
 
 
-	}
+    }
 
 
 
-	//Withdraw Tokens - For Lenders
+    //Withdraw Tokens - For Lenders
 
-	function WithdrawTokens(address _tokenaddress, uint _amount) public {
+    function WithdrawTokens(address _tokenaddress, uint _amount) public {
 
-		require(_amount > 0, "Amount cannot be 0");
-		require(whitelistedToken(_tokenaddress) == true, "Not a whitelisted Token");
+        require(_amount > 0, "Amount cannot be 0");
+        require(whitelistedToken[_tokenaddress] == true, "Not a whitelisted Token");
 
-		uint balance = depositBalance[_tokenaddress][msg.sender];
+        uint balance = depositBalance[_tokenaddress][msg.sender];
 
-		require(balance > 0, "No balance to withdraw");
-		require(_amount <= balance, "Deposit is  less than requested Withdraw amount");
+        require(balance > 0, "No balance to withdraw");
+        require(_amount <= balance, "Deposit is  less than requested Withdraw amount");
 
-		if(balance > 0 && balance >= _amount) {
+        if(balance > 0 && balance >= _amount) {
 
-			IERC20(_tokenaddress).safeTransfer(msg.sender, _amount);
+            IERC20(_tokenaddress).safeTransfer(msg.sender, _amount);
 
-			depositBalance[_tokenaddress][msg.sender] = depositBalance[_tokenaddress][msg.sender] - _amount;
+            depositBalance[_tokenaddress][msg.sender] = depositBalance[_tokenaddress][msg.sender] - _amount;
 
-		}  
+        }  
 
-	}
+    }
 
 
-	function setCreditLimit(address _tokenaddress, address _borroweraddress, uint _amount) public {
+    function setCreditLimit(address _tokenaddress, address _borroweraddress, uint _amount) public {
 
-		require(whitelistedToken(_tokenaddress) == true, "Not a whitelisted Token");
-		require(msg.sender == Admin, "Only Admin can call this function");
-		require(_amount > 0, "Amount cannot be 0");
+        require(whitelistedToken[_tokenaddress] == true, "Not a whitelisted Token");
+        require(msg.sender == Admin, "Only Admin can call this function");
+        require(_amount > 0, "Amount cannot be 0");
 
-		creditLimit[_tokenaddress][_borroweraddress] = _tokenaddress;
+        creditLimit[_tokenaddress][_borroweraddress] = _amount;
 
-	}
+    }
 
 
-	//Borrow Tokens - For Borrowers
+    //Borrow Tokens - For Borrowers
 
-	function BorrowTokens(address _tokenaddress, uint _amount) public {
+    function BorrowTokens(address _tokenaddress, uint _amount) public {
 
-		require(_amount > 0, "Amount cannot be 0");
-		require(whitelistedToken(_tokenaddress) == true, "Only whitelisted tokens can be borrowed");
+        require(_amount > 0, "Amount cannot be 0");
+        require(whitelistedToken[_tokenaddress] == true, "Only whitelisted tokens can be borrowed");
 
-		pool_balance = IERC20(_tokenaddress).balanceOf(address(this))
+        uint pool_balance = IERC20(_tokenaddress).balanceOf(address(this));
 
-		require(pool_balance >= _amount, "Borrowing Suspended temporarily");
+        require(pool_balance >= _amount, "Borrowing Suspended temporarily");
 
-		limit = creditLimit[_tokenaddress][msg.sender];
+        uint limit = creditLimit[_tokenaddress][msg.sender];
 
-		require(limit != 0, "You don't have a Credit Limit");
+        require(limit != 0, "You don't have a Credit Limit");
 
-		uint bAmount = borrowedAmount[_tokenaddress][msg.sender];
+        uint bAmount = borrowedAmount[_tokenaddress][msg.sender];
 
-		uint totalCredit = bAmount + _amount;
+        uint totalCredit = bAmount + _amount;
 
-		require(totalCredit <= limit, "Requested amount exceeds your Credit Limit");
+        require(totalCredit <= limit, "Requested amount exceeds your Credit Limit");
 
 
-		IERC20(_tokenaddress).safeTransfer(msg.sender, _amount);
+        IERC20(_tokenaddress).safeTransfer(msg.sender, _amount);
 
-		borrowedAmount[_tokenaddress][msg.sender] = borrowedAmount[_tokenaddress][msg.sender] + _amount;
+        borrowedAmount[_tokenaddress][msg.sender] = borrowedAmount[_tokenaddress][msg.sender] + _amount;
 
-		if(!hasBorrowed[msg.sender]) {
+        if(!hasBorrowed[msg.sender]) {
 
-			borrowers.push(msg.sender);
-		}	
+            borrowers.push(msg.sender);
+        }   
 
-		//Update borrower status
+        //Update borrower status
 
-		hasBorrowed[msg.sender] = true;
+        hasBorrowed[msg.sender] = true;
  
-	}
+    }
 
 
-	//Repay Tokens - For Borrowers
+    //Repay Tokens - For Borrowers
 
-	function RepayTokens(address _tokenaddress, uint _amount) public {
-
-
-		//require amount greater than 0. Require is a function in solidity.
-		require(_amount > 0, "Amount cannot be 0");
-		require(whitelistedToken(_tokenaddress) == true, "Not a whitelisted token");
-
-		uint bAmount = borrowedAmount[_tokenaddress][msg.sender];
-
-		require(bAmount <= _amount, "Repayment amount greater than current due");
+    function RepayTokens(address _tokenaddress, uint _amount) public {
 
 
-		//Transfer Dai Tokens
-		IERC20(_tokenaddress).safeTransferFrom(msg.sender, address(this), _amount);
+        //require amount greater than 0. Require is a function in solidity.
+        require(_amount > 0, "Amount cannot be 0");
+        require(whitelistedToken[_tokenaddress] == true, "Not a whitelisted token");
 
-		//Update borrow balance
+        uint bAmount = borrowedAmount[_tokenaddress][msg.sender];
 
-		borrowedAmount[_tokenaddress][msg.sender] = borrowedAmount[_tokenaddress][msg.sender] - _amount;
+        require(bAmount <= _amount, "Repayment amount greater than current due");
 
 
-	}
+        //Transfer Dai Tokens
+        IERC20(_tokenaddress).safeTransferFrom(msg.sender, address(this), _amount);
+
+        //Update borrow balance
+
+        borrowedAmount[_tokenaddress][msg.sender] = borrowedAmount[_tokenaddress][msg.sender] - _amount;
+
+
+    }
 
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,6 @@
 
-const TDai = artifacts.require("TDai");
+const tDai = artifacts.require("tDai");
 const BasicCredit = artifacts.require("BasicCredit");
-
-
-module.exports = function (deployer) {
-  deployer.deploy(greeting);
-};
 
 
 
@@ -14,19 +9,15 @@ module.exports = async function(deployer, network, accounts) {
 
   //Deploy test Dai Token
 
-  await deployer.deploy(TDai)
+  await deployer.deploy(tDai)
   const tdai = await TDai.deployed()
 
 
   //Deploy Token Farm
   
-  await deployer.deploy(BasicCredit, tdai.address)
+  await deployer.deploy(BasicCredit)
   const basicCredit = await BasicCredit.deployed()
 
   
-  // Transfer 1000 test Dai to Investor (i.e to 2nd account in Ganache)
-
-  await tdai.MintToken(accounts[1], '1000000000000000000000') 
-
 
 };
